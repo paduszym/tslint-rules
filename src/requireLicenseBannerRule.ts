a946904e2afa1cfcabbd98e1ac579db36218ae03
Mateusz Paduszyński <mpaduszynski@opi.org.pl>
2018-04-27 11:08:16 +0200

    init

--- src/requireLicenseBannerRule.ts
+++ src/requireLicenseBannerRule.ts
@@ -1,4 +1,21 @@
 /**
+ * @license
+ * To oprogramowanie jest własnością
+ *
+ * OPI - Ośrodek Przetwarzania Informacji,
+ * Al. Niepodległości 188B, 00-608 Warszawa
+ * Numer KRS: 0000127372
+ * Sąd Rejonowy dla m. st. Warszawy w Warszawie XII Wydział
+ * Gospodarczy KRS
+ * REGON: 006746090
+ * NIP: 525-000-91-40
+ *
+ * Wszystkie prawa zastrzeżone. To oprogramowanie może być używane tylko
+ * zgodnie z przeznaczeniem. OPI nie odpowiada za ewentualne wadliwe
+ * działanie kodu.
+ */
+
+/**
  * @see https://github.com/angular/material2/blob/master/tools/tslint-rules/requireLicenseBannerRule.ts
  */
 import {readFileSync} from "fs";
