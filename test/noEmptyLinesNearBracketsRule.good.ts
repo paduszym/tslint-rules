23fe92787342a78451a89c12ac68dcaaa87f57dd
Mateusz Paduszyński <mpaduszynski@opi.org.pl>
2018-05-07 15:03:03 +0200

    tests

--- test/noEmptyLinesNearBracketsRule.good.ts
+++ test/noEmptyLinesNearBracketsRule.good.ts
@@ -3,3 +3,44 @@
  *
  * Example license banner
  */
+class AClass {
+
+    a: any;
+}
+
+interface AInterface {
+
+    b: any;
+}
+
+type AType = {
+    c: any;
+};
+
+const x: any = {
+    x: 1,
+    y: "a",
+    z: false,
+};
+
+function f(a: any, b: any, c: any): void {
+    const d: any = [
+        "a",
+        "b",
+        "c",
+    ];
+
+    if (a === 1) {
+        console.log("OK");
+    }
+}
+
+function g(): void {
+    console.log("f()");
+
+    f(
+        1,
+        2,
+        3,
+    );
+}
